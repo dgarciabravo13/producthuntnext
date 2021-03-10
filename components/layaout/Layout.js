079599c639670859118c9f1afb53703d6f04de89
David Garcia <dgarciabravo13@gmail.com>
2021-03-10 14:37:15 +0100

    main page done

--- components/layaout/Layout.js
+++ components/layaout/Layout.js
@@ -10,7 +10,8 @@ const Layout = (props) => {
         styles={css`
           :root {
             --gris: #3d3d3d;
-            --gris2: #6f6f6df;
+            --gris2: #6f6f6f;
+            --gris3: #e1e1e1;
             --naranja: #da552f;
           }
           html {
@@ -25,12 +26,21 @@ const Layout = (props) => {
           body {
             font-size: 1.6rem;
             line-height: 1.5;
+            font-family: 'PT Sans', sans-serif;
           }
           h1,
           h2,
           h3 {
             margin: 0 0 2rem 0;
             line-height: 1.5;
+          }
+
+          h1, h2{
+            font-family: 'Roboto Slab', serif;
+            font-weight:700;
+          }
+          h3{
+            font-family: 'PT Sans', sans-serif;
           }
           ul {
             list-style: none;
@@ -51,6 +61,12 @@ const Layout = (props) => {
           integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
           crossOrigin="anonymous"
         />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/static/css/app.css" rel="stylesheet"/>
       </Head>
       <Header />
       <main>{props.children}</main>
---
+++ pages/nuevo-producto.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layaout/Layout";
+
+const NuevoProducto = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Nuevo Producto</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default NuevoProducto;
