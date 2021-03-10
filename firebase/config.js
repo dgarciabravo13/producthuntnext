eca0b2e94f1fe3527771681d7666267cbcc4e540
David Garcia <dgarciabravo13@gmail.com>
2021-03-10 19:07:15 +0100

    add firebase

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1qhXoyQFOVPibd6bmJmpLIFVKOBjCQAw",
+    authDomain: "product-hunt-dd07c.firebaseapp.com",
+    projectId: "product-hunt-dd07c",
+    storageBucket: "product-hunt-dd07c.appspot.com",
+    messagingSenderId: "675022330842",
+    appId: "1:675022330842:web:fa5aba90619bdc937d3ee3"
+  };
+
+  export default firebaseConfig;
